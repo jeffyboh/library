87054c0ab08c6fb38063006ff20384116c95920b
Jeff Shannon <jtshannon@gmail.com>
2017-12-23 02:31:09 -0500

    Added gulp and nodemon

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var express = require('express');
 
 var app = express();
-var port = 5000;
+var port = process.env.port || 8080;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.send('Hello world!');
 });
 
-app.get('/books', function(req, res){
+app.get('/books', function(req, res) {
     res.send('Hello books!');
 });
 
-app.listen(5000, function(err){
+app.listen(port, function(err) {
     console.log('running server on port ' + port);
 });
