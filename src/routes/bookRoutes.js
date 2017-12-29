01ec18dcb13ddae30f0c2d5905537661a9790c6f
Jeff Shannon <jtshannon@gmail.com>
2017-12-29 03:21:49 -0500

    added authentication

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -4,6 +4,14 @@ var objectId = require('mongodb').ObjectID;
 var bookRouter = express.Router();
 
 var router = function(nav) {
+    /*
+    bookRouter.use(function(req, res, next) {
+        if (!req.user) {
+            res.redirect('/');
+        }
+        next();
+    });
+    */
     bookRouter.route('/').get(function(req, res) {
         var url = 'mongodb://localhost:27017/libraryApp';
 
